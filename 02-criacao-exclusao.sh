0fc0dd2b0e165a0c1ad1f94aa04af0dae93de802
Felipe Correia <felipecorreiatech@gmail.com>
2026-09-18 11:27:32 -0300

    Adiciona script de criação e exclusão

---
+++ 02-criacao-exclusao.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# ==========================================
+# LINUX BASH - CRIAÇÃO E EXCLUSÃO
+# ==========================================
+
+# mkdir
+# Cria um novo diretório.
+mkdir laboratorio
+
+# touch
+# Cria um arquivo vazio.
+touch arquivo.txt
+
+# echo
+# Cria um arquivo com conteúdo.
+echo "Olá, Linux!" > mensagem.txt
+
+# rm
+# Remove um arquivo.
+rm arquivo.txt
+
+# rm -r
+# Remove um diretório e seu conteúdo.
+rm -r laboratorio
+
+# ==========================================
+# ATENÇÃO:
+# rm não utiliza uma lixeira.
+# Arquivos removidos podem não ser recuperáveis.
+# ==========================================
